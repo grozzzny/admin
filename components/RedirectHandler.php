4cfdd64e372df7990513b8ef349b1a67b3366f9e
grozzzny <grozzzny@list.ru>
2020-07-01 23:27:42 +0200

    fix bug

--- components/RedirectHandler.php
+++ components/RedirectHandler.php
@@ -9,13 +9,13 @@ use yii\base\BaseObject;
 
 class RedirectHandler extends BaseObject
 {
-    public function run($event)
+    public static function run($event)
     {
-        if(!Yii::$app->request->isSecureConnection){
-            $url = Yii::$app->request->absoluteUrl;
-            $url = str_replace('http:', 'https:', $url);
-            Yii::$app->response->redirect($url);
-            Yii::$app->end();
-        }
+        if(Yii::$app->request->isSecureConnection) return;
+
+        $url = Yii::$app->request->absoluteUrl;
+        $url = str_replace('http:', 'https:', $url);
+        Yii::$app->response->redirect($url);
+        Yii::$app->end();
     }
 }
