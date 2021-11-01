b98b15c3279815b9ad5ae0eef0d36cae32484389
grozzzny <grozzzny@list.ru>
2021-11-01 20:21:23 +0200

    change form

--- modules/question/models/AdminQuestionSearch.php
+++ modules/question/models/AdminQuestionSearch.php
@@ -46,6 +46,11 @@ class AdminQuestionSearch extends AdminQuestion
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => [
+                'defaultOrder' => [
+                    'created_at' => SORT_DESC
+                ]
+            ]
         ]);
 
         $this->load($params);
